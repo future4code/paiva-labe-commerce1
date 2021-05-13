309dd8dd593c848eac788b52606609776b7ec84f
alvesgarrido <matheus.agarrido@gmail.com>
2021-05-13 11:20:04 -0300

    State App

--- src/App.js
+++ src/App.js
@@ -7,8 +7,29 @@ import Carrinho from './components/Carrinho/Carrinho'
 
 export default class App extends React.Component {
   state = {
-
-  }
+    produtos: [
+      {
+        id: 1,
+        name: "Camisa Marte",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/200/200"
+      },
+      {
+        id: 2,
+        name: "T-shirt lua",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/200/200"
+      },
+      {
+        id: 3,
+        name: "Regata plutão",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/200/200"
+      }
+    ],
+    inputValue: ""
+  };
+  
   render() {
     return (
       <MainContainer>
