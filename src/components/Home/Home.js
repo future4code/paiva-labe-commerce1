db61b8cfac5c50d42545fec0050ac98524eed9ff
Adrian Américo <1fnadrian1@gmail.com>
2021-05-13 19:18:33 -0300

    ALELUIA

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,52 @@
 import React from 'react'
 import { HomeContainer } from '../../styles/styled'
-import Card from '../Card/Card'
-import camiseta1 from '../../img/camisetas/camiseta1.png'
-import camiseta2 from '../../img/camisetas/camiseta2.png'
-import camiseta3 from '../../img/camisetas/camiseta3.png'
-import camiseta4 from '../../img/camisetas/camiseta4.png'
-import camiseta5 from '../../img/camisetas/camiseta5.png'
+
 export default class Home extends React.Component {
 
     state = {
         ordem: "crescente"
     }
     render() {
-        console.log(this.props)
-
         const produtoFiltrado = this.props.produtos
             .filter((item) => {
                 if (
-                item.name.includes(this.props.inputValue)
+                    item.name.toLowerCase().includes(this.props.inputValue.toLowerCase())
                 ) {
-                return item.id;
+                    return item.id;
                 }
             })
             .filter((item) => {
                 if (item.value >= this.props.inputValorMinimo) {
-                return item.id;
+                    return item.id;
                 }
             })
             .filter((item) => {
                 if (item.value <= this.props.inputValorMaximo) {
-                return item.id;
+                    return item.id;
                 }
-        });
+            });
+
+
+        const produtoOrdemCrescente = produtoFiltrado.sort((a, b) => {
+            return a.value < b.value ? -1 : a.value > b.value ? 1 : 0;
+        })
+        console.log('Ordem Crescente', produtoOrdemCrescente)
+
+
+
+
+        const produtoOrdemDecrescete = produtoFiltrado.reverse((a, b) => {
+            return a.value > b.value ? +1 : a.value < b.value ? 0 : 1;
+        })
+        console.log('Ordem Decrescente', produtoOrdemDecrescete)
+
+
+
+
+
+
+
+        const quantidadeDeItem = (produtoFiltrado.length)
 
         return (
             <HomeContainer>
@@ -40,30 +55,25 @@ export default class Home extends React.Component {
                         <option>Crescente</option>
                         <option>Decrescente</option>
                     </select>
-                    <p>Quantidade de Produtos: 2</p>
+                    <p>Quantidade de Produtos: {quantidadeDeItem}</p>
                 </div>
 
                 <div>
+
                     {produtoFiltrado.map((produto) => {
+                        console.log(produto)
                         return (
-                        <div>
-                            <img src={produto.imageUrl} alt=""></img>
                             <div>
-                            <p>{produto.name}</p>
-                            <p>Valor: {produto.value}</p>
-                            <button>Adicionar ao Carrinho</button>
+                                <img src={produto.imageUrl} alt={produto.name}></img>
+                                <div>
+                                    <p>{produto.name}</p>
+                                    <p>Valor: {produto.value}</p>
+                                    <button>Adicionar ao Carrinho</button>
+                                </div>
                             </div>
-                        </div>
                         );
                     })}
                 </div>
-
-                <Card produtoFoto={camiseta1} produtoNome="aaaaaaa" produtoPreco="aaaa" />
-                <Card produtoFoto={camiseta2} produtoNome="aaaaaaa" produtoPreco="aaaa" />
-                <Card produtoFoto={camiseta3} produtoNome="aaaaaaa" produtoPreco="aaaa" />
-                <Card produtoFoto={camiseta4} produtoNome="aaaaaaa" produtoPreco="aaaa" />
-                <Card produtoFoto={camiseta5} produtoNome="aaaaaaa" produtoPreco="aaaa" />
-
             </HomeContainer>
         )
     }
