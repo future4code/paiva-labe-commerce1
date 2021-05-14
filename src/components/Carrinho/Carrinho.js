2bc85a4d3097bc4c7ade54ec6dbd7de6b67848b4
@elizaoliveira88 <eliza_oliveira88@outlook.com>
2021-05-14 16:45:11 -0300

    implementacaoDoCarrinho

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -3,38 +3,27 @@ import { CarrinhoContainer } from '../../styles/styled'
 
 export default class Carrinho extends React.Component {
 
-    apagaItemDoCarrinho = (itemId) => {
-        const copiaListaDoEstado = this.state.produtos.filter((item) => {
-            return itemId !== item.id
-
-        })
-        this.setState({ produtos: copiaListaDoEstado })
-
-    }
-
     render() {
-        console.log(this.props.produtosCarrinho)
+
+
         return (
 
             <CarrinhoContainer>
                 <h3>Carrinho:</h3>
-                {this.props.produtosCarrinho.map((prodCarrinho) => {
-                    let tamanho = (prodCarrinho.length)
-                    console.log(tamanho)
-                    // switch (prodCarrinho.length) {
-                    //     case value:
-                            
-                    //         break;
-                    
-                    //     default:
-                    //         break;
-                    // }
+                {this.props.produtosCarrinho.filter(function (a) {
+                    return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
+                }, Object.create(null)) .map((prodCarrinho) => {
+
                     return (
-                        <div>
+                        <div key={prodCarrinho.name}>
+                            <p>{prodCarrinho.quantidade}X</p>
                             <p>{prodCarrinho.name}</p>
                             <button onClick={this.apagaItemDoCarrinho}>Remover</button>
                         </div>)
-                })}
+                })
+                
+                }
+                
                 <p>Valor total:R$70</p>
             </CarrinhoContainer>
         )
